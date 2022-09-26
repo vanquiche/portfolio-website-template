b2002350965ad95106b46cc50d6b80bd45c07096
vanquiche <stevevang2010@hotmail.com>
2022-09-26 14:11:38 +0900

    move content data to own file and create template file for new sections

---
+++ components/sections/template.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SectionCard from '@components/SectionCard'
+
+const template = () => {
+  return (
+    // add your id and title
+    <SectionCard id='' title='' >
+      <article>
+        {/* add your content here */}
+      </article>
+    </SectionCard>
+  )
+}
+
+export default template
