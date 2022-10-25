639ba89f5d003214fcaa28577862bb3e4cdb83b0
vanquiche <stevevang2010@hotmail.com>
2022-10-25 15:14:09 +0900

    create background title

---
+++ components/BgTitle.tsx
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react';
+import styles from '@styles/BgTitle.module.sass';
+import useIntersectionObserver from '@hooks/useIntersectionObserver';
+
+interface Props {
+  title: string;
+}
+
+const BgTitle = ({ title }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { isVisible } = useIntersectionObserver(ref);
+  const animateStyle: React.CSSProperties = {
+    opacity: isVisible ? 0.3 : 0,
+    transition: 'opacity 700ms ease-out',
+  };
+
+  return (
+    <header className={styles.container} ref={ref} style={animateStyle}>
+      <h1 className={styles.title}>{title}</h1>
+    </header>
+  );
+};
+
+export default BgTitle;
